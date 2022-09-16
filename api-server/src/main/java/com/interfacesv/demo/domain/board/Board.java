7940b870a8223ddd9e546a70ad89a7ef73e4c825
fing9 <djajsl54@naver.com>
2022-09-16 20:52:04 +0900

    feat:Board Repository

---
+++ api-server/src/main/java/com/interfacesv/demo/domain/board/Board.java
@@ -0,0 +1,40 @@
+package com.interfacesv.demo.domain.board;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String title;
+
+    @Column
+    private String content;
+
+    @Column
+    private String type;
+
+    @Builder
+    public Board(String title, String content, String type) {
+        this.title = title;
+        this.content = content;
+        this.type = type;
+    }
+
+    public void update(String title, String content, String type) {
+        this.title = title;
+        this.content = content;
+        this.type = type;
+    }
+
+}

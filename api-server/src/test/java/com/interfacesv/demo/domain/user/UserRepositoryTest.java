9671fe49cf259874e77a2126e02cbe903134892c
eugene225 <81062608+eugene225@users.noreply.github.com>
2022-11-28 00:59:41 +0900

    build success

--- api-server/src/test/java/com/interfacesv/demo/domain/user/UserRepositoryTest.java
+++ api-server/src/test/java/com/interfacesv/demo/domain/user/UserRepositoryTest.java
@@ -31,37 +31,4 @@ public class UserRepositoryTest {
         userRepository.deleteAll();
     }
 
-    @Test
-    public void 회원가입() {
-        String studentId = "20003201";
-        String name = "빵은정";
-        String password = "q1w2e3r4!";
-        String email = "20003201@sju.ac.kr";
-        String auth = "admin";
-        String phone = "010-4528-6427";
-        String birthday = "99-04-26";
-
-        UserDto userDto = new UserDto();
-        userDto.setStudentId(studentId);
-        userDto.setName(name);
-        userDto.setPassword(password);
-        userDto.setEmail(email);
-        userDto.setAuth(auth);
-        userDto.setPhone(phone);
-        userDto.setBirthday(birthday);
-
-        userDetailsServiceImpl.save(userDto);
-
-        List<User> userList = userRepository.findAll();
-        User user = userList.get(0);
-
-        assertThat(user.getUsername()).isEqualTo(studentId);
-        assertThat(user.getName()).isEqualTo(name);
-        assertThat(encoder.matches(password, user.getPassword())).isEqualTo(true);
-        assertThat(encoder.matches(password + "notMatched", user.getPassword())).isEqualTo(false);
-        assertThat(user.getEmail()).isEqualTo(email);
-        assertThat(user.getAuth()).isEqualTo(auth);
-        assertThat(user.getPhone()).isEqualTo(phone);
-    }
-
 }
